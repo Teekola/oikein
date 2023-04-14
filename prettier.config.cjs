3a2d79be97eb0f82de7787dc41d94f75ed56e02b
Teemu Kerkola <tokerk@utu.fi>
2023-04-14 15:39:00 +0300

    initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	plugins: [require.resolve("prettier-plugin-tailwindcss")],
+	endOfLine: "lf",
+	trailingComma: "es5",
+	tabWidth: 3,
+	printWidth: 120
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true
+  },
+  content: ["./src/**/*.{html, js, jsx, ts,tsx}"],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
@@ -7,11 +11,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
     },
   },
   plugins: [],
