cbb80717c64fac3bb2d4ab18c7864d7cf6113fff
Teemu Kerkola <tokerk@utu.fi>
2023-04-19 23:46:20 +0300

    fix all rooms route handler so that it is dynamic

--- src/app/api/room/all/route.ts
+++ src/app/api/room/all/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import { redis } from "src/lib/redis/redis";
 
-export async function GET() {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function GET(req: Request) {
    // Get all the users if the set exists and empty string array if it does not exist
    const allRooms = await redis.keys("room-*");
 
