f7c7c0b3fb9e4a0b21dfd6cc5330855b63549127
Teemu Kerkola <teemu.kerkola@gmail.com>
2023-06-04 19:19:37 +0300

    update packages, improve project config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-	plugins: [require.resolve("prettier-plugin-tailwindcss")],
+	plugins: [
+		"@trivago/prettier-plugin-sort-imports",
+		"prettier-plugin-tailwindcss" // MUST come last
+	],
 	endOfLine: "lf",
 	trailingComma: "es5",
 	tabWidth: 3,
-	printWidth: 90
+	printWidth: 90,
+	importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	tailwindFunctions: ['clsx']
 };
