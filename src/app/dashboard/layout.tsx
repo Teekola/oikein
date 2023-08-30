81b9719c88acb333057abd808f580bd411957885
Teemu Kerkola <tokerk@utu.fi>
2023-08-30 21:46:17 +0300

    clear old code

--- src/app/dashboard/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-export default function DashboardLayout({ children }: PropsWithChildren) {
-   return (
-      <>
-         <div className="flex min-h-screen bg-slate-100">{children}</div>
-      </>
-   );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import RoomForm from "./roomForm";
-
 export default function HomePage() {
    return (
       <main className="flex min-h-screen bg-slate-100 text-slate-900">
          <div className="mx-auto my-auto flex h-full w-full max-w-xl flex-1 flex-col items-center justify-center gap-5 rounded-lg bg-slate-200 p-10">
             <h1 className="text-6xl font-bold">Oikein</h1>
-            <RoomForm />
          </div>
       </main>
    );
