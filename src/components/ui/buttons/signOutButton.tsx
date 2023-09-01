3540be727cd8cc30dd7451bfd041cb1edd701993
Teemu Kerkola <tokerk@utu.fi>
2023-09-01 19:04:59 +0300

    add shadcnui button

--- src/components/ui/buttons/signOutButton.tsx
+++ src/components/ui/buttons/signOutButton.tsx
@@ -1,20 +1,9 @@
 "use client";
 
-import clsx from "clsx";
 import { signOut } from "next-auth/react";
 
-export default function SignOutButton({ fullWidth }: { fullWidth?: boolean }) {
-   return (
-      <button
-         type="button"
-         className={clsx(
-            "flex-grow-0 rounded bg-white p-3 text-slate-900 shadow-sm hover:bg-slate-50",
-            fullWidth && "w-full",
-            !fullWidth && "w-fit"
-         )}
-         onClick={() => signOut()}
-      >
-         Kirjaudu ulos
-      </button>
-   );
+import { Button } from "./button";
+
+export default function SignOutButton() {
+   return <Button onClick={() => signOut()}>Kirjaudu ulos</Button>;
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,40 +1,36 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { fontFamily } = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js, jsx, ts,tsx}"],
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
-  future: {
-    hoverOnlyWhenSupported: true
-  },
-  darkMode: "class",
-  theme: {
-    extend: {
-      screens: {
-        xs: "480px",
+   darkMode: ["class"],
+   content: [
+      "./pages/**/*.{ts,tsx}",
+      "./components/**/*.{ts,tsx}",
+      "./app/**/*.{ts,tsx}",
+      "./src/**/*.{ts,tsx}",
+   ],
+   theme: {
+      container: {
+         center: true,
+         padding: "2rem",
+         screens: {
+            "2xl": "1400px",
+         },
       },
-      fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans]
+      extend: {
+         keyframes: {
+            "accordion-down": {
+               from: { height: 0 },
+               to: { height: "var(--radix-accordion-content-height)" },
+            },
+            "accordion-up": {
+               from: { height: "var(--radix-accordion-content-height)" },
+               to: { height: 0 },
+            },
+         },
+         animation: {
+            "accordion-down": "accordion-down 0.2s ease-out",
+            "accordion-up": "accordion-up 0.2s ease-out",
+         },
       },
-      fontSize: {
-        '2xs': '0.75rem',
-        xs: '0.875rem',
-        sm: '1rem',
-        base: ['1.125rem'],
-        lg: '1.25rem',
-        xl: '1.5rem',
-        '2xl': '1.875rem',
-        '3xl': '2.25rem',
-        '4xl': '3rem',
-        '5xl': '4rem',
-        '6xl': '5rem',
-      },
-    },
-  },
-  plugins: [],
-}
+   },
+   plugins: [require("tailwindcss-animate")],
+};
